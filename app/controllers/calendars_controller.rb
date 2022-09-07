6a03d0f28fbf354e56ad93d481cbfe43872eeb85
akifumiyamane <akifumiyamane@icloud.com>
2022-09-07 21:27:50 +0900

    issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,14 +8,14 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan: params[:plan])
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -42,7 +42,7 @@ class CalendarsController < ApplicationController
       end
 
       days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wday_num }        
-      
+      @week_days.push(days)
     end    
   end  
 end  
